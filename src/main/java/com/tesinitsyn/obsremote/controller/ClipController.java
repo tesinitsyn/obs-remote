d78e1781aaecbc7550274e61259da271897ddad0
tesinitsyn <sitiev174@gmail.com>
2025-12-31 14:39:44 +0300

    update

--- src/main/java/com/tesinitsyn/obsremote/controller/ClipController.java
+++ src/main/java/com/tesinitsyn/obsremote/controller/ClipController.java
@@ -1,4 +1,21 @@
 package com.tesinitsyn.obsremote.controller;
 
+import com.tesinitsyn.obsremote.service.HotkeyService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClipController {
+
+    private final HotkeyService hotkeyService;
+
+    public ClipController(HotkeyService hotkeyService) {
+        this.hotkeyService = hotkeyService;
+    }
+
+    @PostMapping("/clip")
+    public String makeClip() {
+        hotkeyService.makeClip();
+        return "CLIP OK";
+    }
 }
