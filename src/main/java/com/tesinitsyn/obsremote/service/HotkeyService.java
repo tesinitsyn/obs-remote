395a303b6100b7629052778f289d296299741314
tesinitsyn <sitiev174@gmail.com>
2025-12-31 14:51:58 +0300

    update

--- src/main/java/com/tesinitsyn/obsremote/service/HotkeyService.java
+++ src/main/java/com/tesinitsyn/obsremote/service/HotkeyService.java
@@ -2,28 +2,15 @@ package com.tesinitsyn.obsremote.service;
 
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
-import java.awt.event.KeyEvent;
-
 @Service
 public class HotkeyService {
 
     public void makeClip() {
         try {
-            Robot robot = new Robot();
-
-            robot.keyPress(KeyEvent.VK_CONTROL);
-            robot.keyPress(KeyEvent.VK_ALT);
-            robot.keyPress(KeyEvent.VK_C);
-
-            robot.delay(100);
-
-            robot.keyRelease(KeyEvent.VK_C);
-            robot.keyRelease(KeyEvent.VK_ALT);
-            robot.keyRelease(KeyEvent.VK_CONTROL);
-
-        } catch (AWTException e) {
-            throw new RuntimeException("Cannot press hotkeys", e);
+            new ProcessBuilder("cmd", "/c", "C:\\obs-remote\\idk.ahk").start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
+
